2a34ff3cbaafbf88a7edae0ac0d3d428da79fe8f
yakup-kalkan <yakup.kalkan@hotmail.de>
2025-01-09 16:43:36 +0100

    Create event was saved to the DB

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import {Routes, Route} from "react-router-dom";
-import {useAuth} from "./context/AuthProvider";
+import { Routes, Route } from "react-router-dom";
+import { useAuth } from "./context/AuthProvider";
 import SignIn from "./components/SignIn.jsx";
 import SignUp from "./components/SignUp.jsx";
 import NotFound from "./components/NotFound.jsx";
@@ -11,31 +11,37 @@ import EventsList from "./components/EventsList.jsx";
 import ContentContainer from "./components/ContentContainer.jsx";
 
 function App() {
-    const {isAuthenticated} = useAuth();
+  const { isAuthenticated } = useAuth();
 
-    return (
-        <div className="flex flex-col min-h-screen">
-            <Header/>
-            <ContentContainer>
-                <Routes>
-                    <Route path="/" element={<EventsList/>}/>
-                    <Route path="signin" element={isAuthenticated ? <EventsList/> : <SignIn/>}/>
-                    <Route path="signup" element={isAuthenticated ? <EventsList/> : <SignUp/>}/>
-                    <Route
-                        path="eventcard/:eventId"
-                        element={isAuthenticated ? <EventsList/> : <EventDetail/>}
-                    />
-                    <Route
-                        path="eventcreate"
-                        element={isAuthenticated ? <EventsList/> : <EventCreate/>}
-                    />
-                    {/* 404 Not Found */}
-                    <Route path="*" element={<NotFound/>}/>
-                </Routes>
-            </ContentContainer>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <ContentContainer>
+        <Routes>
+          <Route path="/" element={<EventsList />} />
+          <Route
+            path="signin"
+            element={isAuthenticated ? <EventsList /> : <SignIn />}
+          />
+          <Route
+            path="signup"
+            element={isAuthenticated ? <EventsList /> : <SignUp />}
+          />
+          <Route
+            path="eventcard/:eventId"
+            element={!isAuthenticated ? <EventsList /> : <EventDetail />}
+          />
+          <Route
+            path="eventcreate"
+            element={!isAuthenticated ? <EventsList /> : <EventCreate />}
+          />
+          {/* 404 Not Found */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ContentContainer>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
